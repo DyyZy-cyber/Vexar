2bb549f2a1935540c0d6b769c5d874efb9a8c0d0
Dyyhaha <siuuumodesad3636@gmail.com>
2025-12-07 21:27:00 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  BOT_TOKEN: "8462463147:AAHd6IoSJJLK48TmBZjmUK8KTkbKzn4QJKw",
+  OWNER_LINK: "https://t.me/Dyshaha",
+  OWNER_ID: "8116828553",
+  CHANNEL_LINK: "https://t.me/NewchDy",
+  PHOTO_URL: "https://files.catbox.moe/w7mdys.jpg",
+  MUSIK_URL: "https://files.catbox.moe/f2hzef.mp3"
+};
